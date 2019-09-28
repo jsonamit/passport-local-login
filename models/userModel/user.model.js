b4f31c9a6d2475de52703feb36ebf9facb9a921e
jsonamit <jsonamit@gmail.com>
2019-09-28 15:54:34 +0530

    passport login

---
+++ models/userModel/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    }
+});
+
+const users=mongoose.model('users',userSchema);
+
+module.exports = users;
